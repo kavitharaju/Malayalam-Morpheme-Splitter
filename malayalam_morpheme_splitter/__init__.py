90093208f50901d741c14c86968bd21b12dd1907
glad.e <gladysann1307@gmail.com>
2024-08-29 14:46:23 +0530

    Minor change in the init file

--- malayalam_morpheme_splitter/__init__.py
+++ malayalam_morpheme_splitter/__init__.py
@@ -1,1 +1,6 @@
-from malayalam_morpheme_splitter.DB_interface import *
+"""
+malayalam_morpheme_splitter
+
+A Python package for performing morphological analysis of Malayalam words.
+"""
+from malayalam_morpheme_splitter.db_interface import read_all_examples, morph_analysis, db_entry, root_word_entry
