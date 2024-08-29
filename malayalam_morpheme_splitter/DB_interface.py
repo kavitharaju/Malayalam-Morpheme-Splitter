43e6a57498bc7a13e620f8dfadafa5d66ce22e78
glad.e <gladysann1307@gmail.com>
2024-08-29 14:53:51 +0530

    Minor change in db_entry function

--- malayalam_morpheme_splitter/DB_interface.py
+++ malayalam_morpheme_splitter/DB_interface.py
@@ -1,16 +1,29 @@
+"""
+This module handles morphological analysis for Malayalam words.
+It provides functions for reading the database, splitting words,
+and managing database entries for examples and root_word_lookup.
+"""
 import re
 import os
 from data.morph_examples import examples
 from data.malayalam_words import root_word_lookup
 
-
-
 def read_all_examples():
+    """Returns the entire examples dictionary."""
     return examples
 
 
 
 def find_morph(word):
+    """
+    Finds the morphological components of a word.
+    
+    Args:
+        word (str): The word to analyze.
+    
+    Returns:
+        list: A list containing the root word and its suffix.
+    """
     if not word:
         return [word, '']
     for w in examples:
@@ -31,6 +44,15 @@ def find_morph(word):
 
 
 def morph_analysis(sentence):
+    """
+    Performs morphological analysis on a given word.
+    
+    Args:
+        word (str): The word or sentence to analyze.
+    
+    Returns:
+        list: A list of lists containing the analysis of each part of the word or sentence.
+    """
     words = re.findall(r'[\u0080-\uFFFF]+|\w+', sentence)
     analyzed_words = []
     for word in words:
@@ -42,7 +64,7 @@ def morph_analysis(sentence):
                 break
             temp = find_morph(root)
             root = temp[0]
-            analyzed_word.insert(0, temp[1]) 
+            analyzed_word.insert(0, temp[1])
         analyzed_words.append(analyzed_word)
     return analyzed_words
 
@@ -50,23 +72,33 @@ def morph_analysis(sentence):
 
 
 def db_entry(inp):
+    """
+    Adds entries in the examples database.
+    
+    Args:
+        inp (dict): A dictionary where keys are words and values are the analyzed forms.
+    
+    Raises:
+        ValueError: If the entry would create redundancy.
+    """
     for word, new_answer in inp.items():
+        entries = ''
         analysed_word = find_morph(word)
         if new_answer == analysed_word and word in examples:
             raise ValueError('This entry would create redundancy')
         examples[word] = new_answer
+        entries += f"'{word}': {new_answer},\n"
         try:
             with open('data/morph_examples.py', 'r+', encoding='utf-8') as db:
                 db.seek(0, os.SEEK_END)
-                pos = db.tell()  
+                pos = db.tell()
                 while pos > 0:
                     pos -= 1
                     db.seek(pos, os.SEEK_SET)
                     if db.read(1) == '}':
                         break
                 db.seek(pos - 1, os.SEEK_SET)
-                for word, new_answer in inp.items():
-                    db.write(f"'{word}' : {new_answer},")
+                db.write(entries)
                 db.write("\n}")
         except:
             pass
@@ -74,6 +106,15 @@ def db_entry(inp):
 
 
 def root_word_entry(word):
+    """
+    Adds a new root word to the root_word_lookup list.
+    
+    Args:
+        word (str): The root word to add.
+    
+    Raises:
+        ValueError: If the entry would create redundancy.
+    """
     if word in root_word_lookup:
         raise ValueError('This entry would create redundancy')
     root_word_lookup.append(word)
@@ -90,12 +131,3 @@ def root_word_entry(word):
             f.write(f'"{word}",\n]')
     except:
         pass
-
-
-
-
-
-
-
-
-            
--- malayalam_morpheme_splitter/install.py
+++ malayalam_morpheme_splitter/install.py
@@ -1,22 +1,27 @@
+"""
+install.py
+
+This module provides an installation script 
+for setting up the necessary data files for 
+the Malayalam Morpheme Splitter. 
+"""
 import os
 import shutil
 import pkg_resources
 
 def main():
-   
-    target_dir = os.getcwd()
-
-   
+    """
+    Copies the 'morph_examples.py' and 'malayalam_words.py' files 
+    from the package data directory to a hidden directory in the user's home directory.
+    """
+    mms_dir = os.path.expanduser("~/.mms_data")
+    os.makedirs(mms_dir, exist_ok=True)
+    path_1 = os.path.join(mms_dir, "morph_examples.py")
+    path_2 = os.path.join(mms_dir, "malayalam_words.py")
     data_file_1 = pkg_resources.resource_filename('malayalam_morpheme_splitter', 'data/morph_examples.py')
     data_file_2 = pkg_resources.resource_filename('malayalam_morpheme_splitter', 'data/malayalam_words.py')
+    shutil.copy2(data_file_1, path_1)
+    shutil.copy2(data_file_2, path_2)
+    print(f'morph_examples.py has been installed to {path_1}')
+    print(f'malayalam_words.py has been installed to {path_2}')
 
-  
-    destinationfile_1 = os.path.join(target_dir, 'morph_examples.py')
-    destinationfile_2 = os.path.join(target_dir, 'malayalam_words.py')
-
-   
-    shutil.copy2(data_file_1, destinationfile_1)
-    shutil.copy2(data_file_2, destinationfile_2)
-
-    print(f'morph_examples.py has been installed to {destinationfile_1}')
-    print(f'malayalam_words.py has been installed to {destinationfile_2}')
